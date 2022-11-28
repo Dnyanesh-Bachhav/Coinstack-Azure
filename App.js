973e9cfaf8fc3c3a6ab36a1367fdffc4e0b4322e
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2022-11-28 18:09:16 +0530

    Upgrading Expo SDK 47...

--- App.js
+++ App.js
@@ -1,13 +1,19 @@
 import react,{useEffect,useState} from 'react';
-import { StyleSheet, Text, View, StatusBar, AppRegistry } from 'react-native';
+import { StyleSheet, Text, View, StatusBar, AppRegistry, LogBox } from 'react-native';
 import { COLORS } from './components/constants';
 import { NavigationContainer } from '@react-navigation/native';
 import Tabs from './Navigation/tabs';
 import NetInfo from '@react-native-community/netinfo';
 import NoInternetScreen from './screens/NoInternetScreen';
 import PortfolioContextProvider from './Contexts/PortfolioContext';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 // import {name as appName} from './app.json';
 // AppRegistry.registerComponent(appName,()=>App);
+
+// LogBox.ignoreLogs([
+// "ViewPropTypes will be removed",
+// "ColorPropType will be removed",
+// ]);
 
 export default function App() {
   const [connected,setConnected] = useState(true);
@@ -19,7 +25,7 @@ export default function App() {
     getNetworkData();
   },[]);
   return (
-    <View style={styles.container}>
+    <GestureHandlerRootView style={styles.container}>
     {
       connected ?
       <>
@@ -34,7 +40,7 @@ export default function App() {
     
   }
     <StatusBar style="auto" backgroundColor={COLORS.primary} styles={styles.statusBarStyle} />
-    </View>
+    </GestureHandlerRootView>
   );
 }
 
